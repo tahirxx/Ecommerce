c4c0f4e22e766d9a4f08a4652b5e830d24f8b6ef
tahirxx <59288568+tahirxx@users.noreply.github.com>
2024-02-11 00:55:19 -0500

    initial files added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Navbar from './components/Navbar'
+import './App.css'
+
+function App() {
+    return (
+    
+      <div>
+        <Navbar />
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+
+// import { MdSearch } from "react-icons/md";
+// import { FaRegHeart } from "react-icons/fa";
+// import { MdOutlineShoppingBag } from "react-icons/md";
+
+export default function Navbar() {
+  return (
+    <>
+      <header className='bg-white flex-wrap items-center shadow-md'>
+        <nav className='border-grap-200 bh-white py-6'>
+            <div className='flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto'>
+                <a href="" className='flex items-center'>
+                    <span className='self-center text-[1.8rem} font-semibold whitespace-nowrap text-black' >
+                        Ashion
+                    </span>
+                </a>
+
+            </div>
+
+        </nav>
+      </header>
+    </>
+  )
+}
